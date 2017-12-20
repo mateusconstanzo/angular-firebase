b244020654ebaba858a7953799aa738486e78330
Mateus Constanzo <mateus.constanzo@entelgy.com>
2017-12-20 08:56:16 -0200

    Add Rascunho da POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
+import * as firebase from 'firebase/app';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
+
+  items: Observable<any[]>;
+
+  itemsRef: AngularFireList<any>;
+
+  constructor(db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+
+    this.items = db.list('/items', ref => ref.limitToFirst(2).orderByKey()).valueChanges()
+
+    //db.list('items').push('aaaaaaaaa');
+
+    //this.items = this.itemsRef.valueChanges();
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function(result) {
+
+      debugger;
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      
+      
+
+    });
+  } 
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FirebaseConfig } from './../environments/firebase.config';
+import { AngularFireModule } from 'angularfire2/index';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(FirebaseConfig, 'angular'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
